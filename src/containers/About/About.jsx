81029340c850d9b385f4ca22f760fa1033d386e5
Musabbir-pr <musabbir313@gmail.com>
2023-05-08 11:26:13 +0600

    added content in About section

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './About.scss'
+import { motion } from 'framer-motion'
+
+import {images} from '../../constants'
+
+const abouts= [
+  {title: "web developer", description: "I'm a good developer who can be great inclution to your team", imgUrl: images.about01},
+  {title: "web Designer", description: "I can design ui/ux ", imgUrl: images.about02},
+  {title: "web animation", description: "I can add stunning animation ", imgUrl: images.about03}
+]
 
 const About = () => {
   return (
-    <div>About</div>
+   <>
+   
+   <h2 className="head-text">
+    I know that 
+    <span>Great Design</span>
+    Means 
+    <span>Great Business</span>
+   </h2>
+
+   <div className="app__profiles">
+
+    {
+      abouts.map((about,index)=>(
+        <motion.div
+        whileInView={{opacity: 1}}
+        whileHover={{scale: 1.1}}
+        transition={{duration: 0.5, type: 'tween'}}
+        className='app__profile-item'
+        key={about.title+ index}
+        >
+          <img src={about.imgUrl} alt={about.title} />
+          <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2> 
+          <h2 className='p-text' style={{marginTop: 10}}>{about.description}</h2> 
+        </motion.div>
+      ))
+    }
+   </div>
+   </>
   )
 }
 
