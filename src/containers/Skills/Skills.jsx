b255e273dc9eaa0a92572c14ba5c05b945a6b88e
musabbir-m <musabbir313@gmail.com>
2023-07-23 13:53:10 +0600

    addede experience section

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -44,6 +44,49 @@ const Skills = () => {
           </motion.div>
         ))}
       </motion.div>
+      <motion.div className='app__skils-exp'>
+        {
+          experience?.map((experience)=>(
+            <motion.div 
+            className='app__skills-exp-item'
+            key={experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+               <motion.div className='app__skills-exp-works'>
+                {
+                  experience.works.map(work=>(
+                    <>
+                      <motion.div
+                      whileInView={{opacity:[0,1]}}
+                      transition={{duration:0.5}}
+                      className='app__skills-exp-work'
+                      data-tip 
+                      data-for={work.name}
+                      key={work.name}
+                      >
+                      <h4 className='bold-text' id={work.name}>{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                      </motion.div>
+                      <Tooltip
+                      anchorId={work.name}
+                      content={work.desc}
+                      className='skills-tooltip'
+                      >
+                       
+                      </Tooltip>
+                    </>
+                  ))
+                }
+               </motion.div>
+            </motion.div>
+
+            
+          ))
+        }
+
+      </motion.div>
     </div>
     </>
   )
